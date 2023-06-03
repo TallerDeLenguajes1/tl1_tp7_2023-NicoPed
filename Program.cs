eff3142fe167698b3d33995c3b29de0993a45c71
NicoPed <nikopedraza10@gmail.com>
2023-06-02 22:56:27 -0300

    Realizado Punto 1

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+
 using EspacioCalculadora;
 internal class Program
 {
@@ -6,49 +7,60 @@ internal class Program
     {
         int opcion;
         double termino = 0.0;
-        string buffer;
+        string? buffer, auxbuff;
+        var calculadora = new Calculadora();
         do
         {
+        Console.WriteLine("========================");
         Console.WriteLine("1- Sumar");
         Console.WriteLine("2- Restar");
         Console.WriteLine("3- Multiplicar");
         Console.WriteLine("4- Dividir");
         Console.WriteLine("5- Limpiar");
         Console.WriteLine("0- Salir");
+        Console.WriteLine("========================");
         Console.Write("Ingrese una opcion: ");
         buffer = Console.ReadLine();
-        if (int.TryParce(buffer, out opcion))
+        auxbuff = buffer;
+        if (int.TryParse(buffer, out opcion))
         {
-            Console.Write("Ingrese un numero: ");
-            buffer = Console.ReadLine();
-            if (double.TryParce(buffer,out termino))
-            {    
-                switch (opcion)
-                {
-                    case 1:
-                    
-                        break;
-                    case 2:
-                        break;
-                    case 3:
-                        break;
-                    case 4:
-                        break;
-                    case 5:
-                        break;
-                    case 0:
-                        break;
-                    default:
-                        Console.WriteLine("===Saliendo===");
-                        break;
+        if (opcion!=0)
+        {          
+            if (opcion != 5)
+            { 
+                Console.Write("Ingrese un numero: ");
+                buffer = Console.ReadLine();
+                
+                if (double.TryParse(buffer,out termino))
+                {    
+                    switch (opcion)
+                    {
+                        case 1:
+                        calculadora.Sumar(termino);
+                            break;
+                        case 2:
+                        calculadora.Restar(termino);
+                            break;
+                        case 3:
+                        calculadora.Multiplicar(termino);
+                            break;
+                        case 4:
+                        calculadora.Dividir(termino);
+                            break;
+                        case 0:
+                            Console.WriteLine("===Saliendo===");
+                            break;
+                        default:
+                            Console.WriteLine("===Ingrese una opcion correcta===");
+                            break;
+                    }
                 }
             }else
             {
-                opcion = 6;
+                calculadora.Limpiar();
             }
-        }else
-        {
-            opcion = 6;
+        }    
+            Console.WriteLine("=====Res: "+ calculadora.resultado+"=====");
         }
         } while (opcion != 0);
     }
